c6bc2b732c5f6b0b8d2ca28a8dabec13c60d2fe9
Ryan <salvat36@gmail.com>
2023-09-19 12:31:17 -0400

    update to include clerk user signin/signup/userbutton

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <main>
-      <h1>BlogZone</h1>
-    </main>
+    <div>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
   )
 }
